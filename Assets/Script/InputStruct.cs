7b0029ec00311db04f7dfadbbf1f91d624f11f2a
winnerinsanjaya <61474469+winnerinsanjaya@users.noreply.github.com>
2022-11-19 12:29:21 +0700

    Player Movement and Enemy Detect
    Player Movement and Enemy Detect

---
+++ Assets/Script/InputStruct.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[System.Serializable]
+public class InputStruct
+{
+    public KeyCode _forward;
+    public KeyCode _backward;
+    public KeyCode _left;
+    public KeyCode _right;
+}
